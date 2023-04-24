e6b310eb12ba68dec28e1ae1654b02e94825f814
zubbysamuel <129983642+zubbysamuel@users.noreply.github.com>
2023-04-24 16:17:16 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,19 +1,19 @@
-students_count = 1000
-print(students_count)
-var_name = 'samuel'
-print(var_name)
-int = 200
-print(type(int))
+# students_count = 1000
+# print(students_count)
+# var_name = 'samuel'
+# print(var_name)
+# int = 200
+# print(type(int))
 
 
-studentCount = 2000
-print(studentCount)
-print(type(studentCount))
+# studentCount = 2000
+# print(studentCount)
+# print(type(studentCount))
 
-x = 2.7
-y = 3.5
-z = x + y
-print(z)
+# x = 2.7
+# y = 3.5
+# z = x + y
+# print(z)
 
 x = 5.0
 print(type(x))
