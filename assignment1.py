40669c1b3f8d0a6ff63778426f236e5560d97822
zubbysamuel <zubbysamuel20@gmail.com>
2023-04-24 22:13:42 -0500

    first commit

---
+++ assignment1.py
@@ -0,0 +1,29 @@
+students_count = 1000
+print(students_count)
+var_name = 'samuel'
+print(var_name)
+int = 200
+print(type(int))
+
+
+studentCount = 2000
+print(studentCount)
+print(type(studentCount))
+
+x = 2.7
+y = 3.5
+z = x + y
+print(z)
+
+x = 5.0
+print(type(x))
+x = bool(x)
+print(x)
+print(type(x))
+
+
+mystr = "DimakaNzubeSamuel"
+last_name = mystr [0:6]
+first_name = mystr [6:11]
+middle_name = mystr [11:]
+print(last_name, first_name, middle_name)
